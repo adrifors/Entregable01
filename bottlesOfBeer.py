1095b437f6f06b4561af4891aa6c0fd33f2d3a63
adrifors <esibemo@gmail.com>
2015-03-11 19:23:26 +0100

    Primer commit

---
+++ bottlesOfBeer.py
@@ -0,0 +1,6 @@
+#Autores: Jose Antonio Adriano Munoz, Isabel Repetto Garcia-Plata
+#Titulo: "99 Bottles of Beer"
+
+for nBotella in range(99,0,-1):
+    print (str(nBotella)+" bottles of beer on the wall, "+str(nBotella)+" bottles of beer. \nTake one down, pass it around, "+str(nBotella-1)+" bottles of beer on the wall.\n")
+     
